be2b78f0312209a2b907c8458937ceb3995d6bd2
Mamun <majanfermun@gmail.com>
2025-12-07 02:48:15 +0600

    setup index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("world");
+});
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
